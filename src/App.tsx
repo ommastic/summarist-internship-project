fb4aa4f22866a0233f7dc8aff636627acf72618a
ommastic <maopebiyi@gmail.com>
2026-05-15 17:19:15 -0400

    started the detail page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import HomePage from './Pages/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ForYouPage from './Pages/ForYouPage';
+import InsideBookPage from './Pages/InsideBookPage';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/for-you" element={<ForYouPage />} />
+        <Route path="/book/:id" element={<InsideBookPage />}/>
       </Routes>
     </Router>
   );
---
+++ src/Pages/InsideBookPage.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+
+
+
+export default function InsideBookPage(){
+    const { id } = useParams();
+    return (
+        <>
+        
+        
+        </>
+    )
+}
