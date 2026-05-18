edb72b758e684abc2202260b6cbebde145a9090f
muheez <muheezopebiyi@gmail.com>
2026-05-18 10:32:39 -0400

    adjusted insidePage to use react icons

--- src/Pages/InsideBookPage.tsx
+++ src/Pages/InsideBookPage.tsx
@@ -2,7 +2,12 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { type Book } from "../types/Book";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TbBulb } from "react-icons/tb";
+import { FaRegStar } from "react-icons/fa6";
+import { FaRegClock, FaRegBookmark } from "react-icons/fa";
+import { HiOutlineMicrophone } from "react-icons/hi";
+import { SlBookOpen } from "react-icons/sl";
+
 import './InsideBookPage.css';
 
 
@@ -29,32 +34,48 @@ export default function InsideBookPage() {
                         <div className='inside-book__author'>{book.author}</div>
                         <div className='inside-book__sub-title'>{book.subTitle}</div>
                         <div className='inside-book--line'></div>
-                        
+
                         <div className="ratings__audio">
                             <div className="ratings__player">
                                 <div className="inside-book__with-average-rating">
-                                    <FontAwesomeIcon icon="star" />
-                                    <span className='inside-book__average-rating'>{book.averageRating}</span>
-                                    <span>({book.totalRating} ratings)</span>
+                                    <FaRegStar className='star__logo' />
+                                    <div className='inside-book__average-rating'>{book.averageRating}</div>
+                                    <div>({book.totalRating} ratings)</div>
                                 </div>
-                                <div>TimerStuff</div>
+                                <div className='timer__information'>
+                                    <FaRegClock className='clock__logo' />
+                                    <div>TimerStuff</div>
+                                </div>
                             </div>
                             <div className='audio__key-ideas'>
-                                <div className='inside-book--rating'> <FontAwesomeIcon className='microphone-logo' icon="microphone" />{book.type}</div>
+                                <div className='microphone__text'>
+                                <HiOutlineMicrophone className="microphone__logo" />
+                                <div className='inside-book--rating'> {book.type}</div>
+                                </div>
 
-                                <div className='main_key--ideas'>{book.keyIdeas} Key ideas</div>
+                                <div className='main_key--ideas'>
+                                    <div className='bulb__key-ideas'>
+                                    <TbBulb className='light__logo' />
+                                    <div>{book.keyIdeas} Key ideas</div>
+                                    </div>
+                                </div>
 
                             </div>
-                        
-                        <div>
-                            <div className='inside-book--line line--gap'></div>
-                            <div className="read__listen--button">
-                                <button className='inside-book__button' onClick={() => navigate(`/player/${book.id}`)}><FontAwesomeIcon className="book_color" icon='book-open' />Read</button>
-                                <button className='inside-book__listen'><FontAwesomeIcon className="microphone__details" icon="microphone" />Listen</button>
+
+                            <div>
+                                <div className='inside-book--line line--gap'></div>
+                                <div className="read__listen--button">
+                                    <button className='inside-book__button' onClick={() => navigate(`/player/${book.id}`)}>
+                                        <div className='read__button'>
+                                            <SlBookOpen className='book__logo' />
+                                            Read
+                                        </div>
+                                    </button>
+                                    <button className='inside-book__listen'><HiOutlineMicrophone className="microphone-logo" />Listen</button>
+                                </div>
                             </div>
                         </div>
-                        </div>
-                        <p className='inside-book--note'><FontAwesomeIcon className='bookmark-logo' icon='bookmark'/>Add title to My Library</p>
+                        <p className='inside-book--note'><FaRegBookmark className='bookmark-logo' />Add title to My Library</p>
                         <p className='inside-book__description'>What's it about?</p>
                         <div className='inside-book__description--details'>
                             {book.tags.map((tag, index) => (<div key={index} className='inside-book-tags'>{tag}</div>))}
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes, faUser, faStar, faMicrophone, faBookOpen, faBookmark} from '@fortawesome/free-solid-svg-icons';
-
-library.add(faTimes, faUser, faStar, faMicrophone, faBookOpen, faBookmark);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
